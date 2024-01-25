0940b71fba602d72eaa23efe277b3d473b996b13
Abdul Quim <abdul.quim99@gmail.com>
2024-01-25 14:47:07 +0500

    Repo Update

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const { ServerConfig, Logger } = require("./config");
 const apiRoutes = require("./routes");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
   console.log("Sucessfully started");
-  Logger.info("Successfully started the server", {});
+  Logger.info("Successfully started the server");
 });
